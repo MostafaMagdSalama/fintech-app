a891e6994e0eb838a6e4e37d3ff78685e895a527
Mostafa Magdy <mostafa.magdy621@gmail.com>
2024-04-06 11:48:18 +0200

    refactor: remove dead code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 import { healthCheck } from '../handlers/healthcheck';
-import passport from 'passport';
-import jwt from 'jsonwebtoken';
 import { createUserController, loginController } from '../controllers/user.controller';
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', healthCheck);
-router.post('/signup',createUserController);
-router.post('/login',loginController )
+router.post('/signup', createUserController);
+router.post('/login', loginController);
 
 export default router;
