e29987fc5ba2fa042e5a1c34fee74965c3b63732
Mostafa Magdy <mostafa.magdy621@gmail.com>
2024-04-06 22:48:42 +0200

    fix: handle error in account controller

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -4,40 +4,60 @@ import { User } from '@prisma/client';
 
 export const openAccountController = async (req: Request & { user: User }, res: Response) => {
   // opening (create) account require userId to link with user table
-  const { id: userId } = req.user;
-  const account = await openAccount({ userId });
-  console.log({ account: account });
-  res.status(201).json(account);
+  try {
+    const { id: userId } = req.user;
+    const account = await openAccount({ userId });
+    console.log({ account: account });
+    res.status(201).json(account);
+  } catch (error) {
+    res.status(400).json({ error: 'Account creation failed' });
+  }
 };
 
 export const depositController = async (req: Request & { user: User }, res: Response) => {
   const { accountId, amount } = req.body;
-  console.log({ accountId, amount });
   // check if the account id is valid and related to loggedin user
-  const account = getAccountById(accountId);
-  if (!account) {
-    return res.status(400).json({ error: 'Invalid account id' });
+  try {
+    const account = getAccountById(accountId);
+    if (!account) {
+      return res.status(400).json({ error: 'Invalid account id' });
+    }
+    // check if the amount is valid greater than 0
+    if (amount <= 0) {
+      return res.status(400).json({ error: 'Invalid amount' });
+    }
+    const transaction = await deposit({ accountId, amount });
+    if (!transaction) {
+      return res.status(400).json({ error: 'Deposit failed' });
+    }
+    return res.status(201).json({ message: 'Deposit successful' });
+  } catch (error) {
+    return res.status(400).json({ error: 'Deposit failed' });
   }
-  // check if the amount is valid greater than 0
-  if (amount <= 0) {
-    return res.status(400).json({ error: 'Invalid amount' });
-  }
-  await deposit({ accountId, amount });
-  return res.status(201).json({ message: 'Deposit successful' });
 };
 
 export const widthdrawController = async (req: Request & { user: User }, res: Response) => {
   const { accountId, amount } = req.body;
   // check if the account id is valid and related to loggedin user
-  const account = await getAccountById(accountId);
-  if (!account) {
-    return res.status(400).json({ error: 'Invalid account id' });
+  try {
+    const account = await getAccountById(accountId);
+    if (!account) {
+      return res.status(400).json({ error: 'Invalid account id' });
+    }
+    // check if the amount to decrement is valid greater than 0
+    // in withdraw we can check if the amount is greater than the balance
+    if (amount <= 0) {
+      return res.status(400).json({ error: 'Please enter a valid amount. Amounts cannot be negative.' });
+    }
+    if (amount > account.balance) {
+      return res.status(400).json({ error: 'Please enter a valid amount. Amounts cannot be less than balance.' });
+    }
+    const transaction = await withdraw({ accountId, amount });
+    if (!transaction) {
+      return res.status(400).json({ error: 'Withdraw failed' });
+    }
+    return res.status(201).json({ message: 'withdraw successful' });
+  } catch (error) {
+    return res.status(400).json({ error: 'Withdraw failed' });
   }
-  // check if the amount to decrement is valid greater than 0
-  // in withdraw we can check if the amount is greater than the balance
-  if (amount <= 0 && amount <= account.balance) {
-    return res.status(400).json({ error: 'Invalid amount' });
-  }
-  await withdraw({ accountId, amount });
-  return res.status(201).json({ message: 'withdraw successful' });
 };
