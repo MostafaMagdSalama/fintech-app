8ae5a89a9239ff9b0b2705f1899a9ab1b17c2ac5
Mostafa Magdy <mostafa.magdy621@gmail.com>
2024-04-06 16:24:01 +0200

    feat: add data validation middleware to user routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { healthCheck } from '../handlers/healthcheck';
 import { createUserController, loginController } from '../controllers/user.controller';
-
+import { createUserSchema, loginSchema } from '../validationSchemas/userValidation';
+import validateRequestSchema from '../middlewares/validationRequestSchema.middleware';
+const app = express();
 const router = express.Router();
-
+app.use(express.json());
 /* GET home page. */
 router.get('/', healthCheck);
-router.post('/signup', createUserController);
-router.post('/login', loginController);
+router.post('/signup', createUserSchema, validateRequestSchema, createUserController);
+router.post('/login', loginSchema, validateRequestSchema, loginController);
 
 export default router;
