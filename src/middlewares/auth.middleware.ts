ab478683407252aeffe601683397d88da61b3f59
Mostafa Magdy <mostafa.magdy621@gmail.com>
2024-04-06 16:26:44 +0200

    feat: add authentication middleware to validate jwt and controll authorization

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,18 +1,19 @@
-import { NextFunction, Request, Response } from "express";
+import { User } from '@prisma/client';
+import { NextFunction, Request, Response } from 'express';
 
 const jwt = require('jsonwebtoken');
 
-function authenticateToken(req:Request, res:Response, next:NextFunction) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
+function authenticateToken(req: Request & { user: User }, res: Response, next: NextFunction) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
-  if (token == null) return res.sendStatus(401)
-
+  if (token == null) return res.sendStatus(401);
+  console.log({ token });
   jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
-    console.log(err)
-    if (err) return res.sendStatus(403)
-    req.user = user
-    next()
-  })
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
 }
 export default authenticateToken;
