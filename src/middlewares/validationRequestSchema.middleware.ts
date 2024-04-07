f788528f50583e0c202c0eabb40c1a2f4ac13b24
Mostafa Magdy <mostafa.magdy621@gmail.com>
2024-04-07 05:02:03 +0200

    refactor: remove console.log

--- src/middlewares/validationRequestSchema.middleware.ts
+++ src/middlewares/validationRequestSchema.middleware.ts
@@ -3,7 +3,6 @@ import { validationResult } from 'express-validator';
 
 const validateRequestSchema = (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
-  console.log('ifjisdjfoijsdfdjo', req.body);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
